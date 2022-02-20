b441c0c32cfc6eb842854d99528af5fd13cdb503
Rovanion Luckey <rovanion.luckey@gmail.com>
2022-02-20 23:39:56 +0100

    Initial commit
    Complete barebones implementation of a dice thrower.

---
+++ src/tärningar/core.cljs
@@ -0,0 +1,55 @@
+(ns ^:figwheel-hooks tärningar.core
+  (:require
+   [goog.dom     :as gdom]
+   [reagent.core :as r  :refer [atom]]
+   [reagent.dom  :as rd]))
+
+(defonce state (atom {:tärningar       []
+                      :antal-tärningar 1}))
+
+(defn kasta-d6 []
+  (+ (rand-int 6) 1))
+
+(defn kasta-d6-obegränsad
+  [antal-tärningar]
+  (let [kast (repeatedly antal-tärningar kasta-d6)
+        antal-sexor (count (filter #(= % 6) kast))
+        utan-sexor (filter #(not= % 6) kast)]
+    (if (= antal-sexor 0)
+      kast
+      (into utan-sexor (kasta-d6-obegränsad (* antal-sexor 2))))))
+
+(defn kastknappstryck
+  [knapp]
+  (swap! state #(assoc % :tärningar (kasta-d6-obegränsad (:antal-tärningar @state)))))
+
+(defn num->tärningstecken
+  [number]
+  (case number
+    1 \⚂
+    2 \⚁
+    3 \⚂
+    4 \⚃
+    5 \⚄
+    6 \⚅))
+
+(defn some-component []
+  [:div
+   [:h1
+    [:button {:on-click #(swap! state update :antal-tärningar dec)} "-"]
+    " " (:antal-tärningar @state) " "
+    [:button {:on-click #(swap! state update :antal-tärningar inc)} "+"]]
+   [:button {:on-click kastknappstryck}
+    "Kasta tärningar"]
+   [:div.dices
+    (for [[index tärning] (map-indexed list (:tärningar @state))]
+      [:span.dice {:key index} (num->tärningstecken tärning)])]])
+
+(defn mountit []
+  (rd/render [some-component]
+             (.-body js/document)))
+
+(mountit)
+
+(defn ^:after-load on-reload []
+  (mountit))
---
+++ test/tärningar/core_test.cljs
@@ -0,0 +1,6 @@
+(ns tärningar.core-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]
+     [tärningar.core :as t]))
+
+(deftest multiply-test)
