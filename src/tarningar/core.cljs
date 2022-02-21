e8660fd275aff14a8f0609a6ec780a68507f4276
Rovanion Luckey <rovanion.luckey@gmail.com>
2022-02-21 14:34:40 +0100

    Put the buttons on the right hand of the screen

--- src/tarningar/core.cljs
+++ src/tarningar/core.cljs
@@ -36,15 +36,15 @@
 (defn some-component []
   [:div
    [:header
+    [:div [:label "Summa"]
+          [:h1 (reduce + (:tärningar @state))]]
     [:div
      [:h1
       [:button {:on-click #(swap! state update :antal-tärningar dec)} "-"]
       " " (:antal-tärningar @state) " "
       [:button {:on-click #(swap! state update :antal-tärningar inc)} "+"]]
      [:button {:on-click kastknappstryck}
-      "Kasta tärningar"]]
-    [:div [:label "Summa"]
-          [:h1 (reduce + (:tärningar @state))]]]
+      "Kasta tärningar"]]]
    [:div.dices
     (for [[index tärning] (map-indexed list (:tärningar @state))]
       [:span.dice {:key index} (num->tärningstecken tärning)])]])
